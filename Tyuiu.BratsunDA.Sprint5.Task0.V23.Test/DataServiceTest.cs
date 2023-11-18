d55bc169b9bb6430b2ec42726c9a636f73bf02f8
BratsunDanila <Tyuiu.BratsunDA@outlook.com>
2023-11-18 19:03:30 +0500

    Add project files.

---
+++ Tyuiu.BratsunDA.Sprint5.Task0.V23.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+using Tyuiu.BratsunDA.Sprint5.Task0.V23.Lib;
+
+namespace Tyuiu.BratsunDA.Sprint5.Task0.V23.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalc()
+        {
+            string path = @"C:\Users\беброн\source\repos\Tyuiu.BratsunDA.Sprint5\Tyuiu.BratsunDA.Sprint5.Task0.V23\bin\Debug\OutPutFileTask0.txt";
+            FileInfo fileinfo = new FileInfo(path);
+            bool fileExist = fileinfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, fileExist);
+        }
+    }
+}
