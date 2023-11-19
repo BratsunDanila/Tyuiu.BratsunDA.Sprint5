d7b9e57ddd9b2cfe6e56111d52473f2e40b44149
BratsunDanila <Tyuiu.BratsunDA@outlook.com>
2023-11-19 17:58:44 +0500

    Task1

---
+++ Tyuiu.BratsunDA.Sprint5.Task1.V1.Test/DataServiceTest.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.BratsunDA.Sprint5.Task1.V1.Lib;
+using System.IO;
+
+namespace Tyuiu.BratsunDA.Sprint5.Task1.V1.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidSaveToFileTextData()
+        {
+            string path = @"C:\Users\беброн\source\repos\Tyuiu.BratsunDA.Sprint5\Tyuiu.BratsunDA.Sprint5.Task1.V1\bin\Debug\OutPutFileTask1.txt";
+
+            FileInfo fileinfo = new FileInfo(path);
+            bool fileExist = fileinfo.Exists;
+
+            bool wait = true;
+            Assert.AreEqual(wait, fileExist);
+        }
+    }
+}
---
+++ Tyuiu.BratsunDA.Sprint5.Task1.V1.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.BratsunDA.Sprint5.Task1.V1.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.BratsunDA.Sprint5.Task1.V1.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("6c7f7e8b-ea1a-4ac3-b37a-3f1f44c8c9a6")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
